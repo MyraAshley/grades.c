75b0724d331c398f1a707d2daabff41d4befe4fd
MyraAshley <47799854+MyraAshley@users.noreply.github.com>
2019-02-22 22:50:04 -0500

    Tried to change line 13
    Constant error on line 13, no idea how to fix it

--- assignment.c
+++ assignment.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
-
+  
     int TOTALPOITNSEARNED;
     int TOTALPOINTSPOSSIBLE;
 int main(void){
@@ -8,8 +8,8 @@ int main(void){
     int pointsEarned[assignments];
     int pointsPossible[assignments];
     for (int x = 0; x < assignments;x++){
-        pointsEarned[x] = get_int ("Points earned on assignment:\n");
-        pointsPossible[x] = get_int ("Points possible earned on assignment:\n");}
-    float divide = pointsEarned/pointsPossible;
+        pointsEarned[x] = get_int ("Points earned on assignment #%d:\n",x+1);
+        pointsPossible[x] = get_int ("Points possible earned on assignment #%d:\n",x+1);}
+    float divide=(pointsEarned/pointsPossible);
     printf ("Final grade: %f\n",divide);
     }
