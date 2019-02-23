ff51c271115137a99f32a1e5ef63991761e6f574
MyraAshley <47799854+MyraAshley@users.noreply.github.com>
2019-02-22 23:50:53 -0500

    Final draft of assignment.c
    constant error on line 13, no idea how to fix it

--- assignment.c
+++ assignment.c
@@ -4,12 +4,12 @@
     int TOTALPOITNSEARNED;
     int TOTALPOINTSPOSSIBLE;
 int main(void){
-    int assignments = get_int("How many different assignments:\n");
-    int pointsEarned[assignments];
-    int pointsPossible[assignments];
-    for (int x = 0; x < assignments;x++){
-        pointsEarned[x] = get_int ("Points earned on assignment #%d:\n",x+1);
-        pointsPossible[x] = get_int ("Points possible earned on assignment #%d:\n",x+1);}
-    float divide=(pointsEarned/pointsPossible);
-    printf ("Final grade: %f\n",divide);
+    int assignments = get_int("How many different assignments:\n"); // asks for the amount assignments in total
+    int pointsEarned[assignments]; // makes an array for points earned
+    int pointsPossible[assignments]; // makes an array for points possible
+    for (int x = 0; x < assignments;x++){ //will loop as long as the integer that was entered is less than the total, and will increase each time by one
+        pointsEarned[x] = get_int ("Points earned on assignment:\n"); // asks for the amount of points earned and puts it into an array
+        pointsPossible[x] = get_int ("Points possible earned on assignment:\n");} // asks for the amount of points possible and puts it into an array
+    float divide= (pointsEarned/ pointsPossible); // divides the points earned from points possible
+    printf ("Final grade: %f\n",divide); // prints the average grade or answer from division problem
     }
