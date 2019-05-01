582fc989e2ef1a27ca20ab07b98bf40a60c9b04a
MyraAshley <47799854+MyraAshley@users.noreply.github.com>
2019-04-30 23:48:45 -0400

    Create math.c

---
+++ math.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int v;
+int i;
+int r;
+
+int main(void){
+   
+    printf ("what is the unknown?/n");
+    char unknown = scanf ("x");
+        if (v == x);
+            int mult = (i +r);
+        if (r == x)
+            int divide = (v/i);
+        if (i == x);
+            int divide = (v/r)
+    int v = get_int ("what is the voltage?/n");
+    int i = get_int ("what is the current?/n");
+    int r = get_int ("what is the resistance?/n");
+}
+
+
