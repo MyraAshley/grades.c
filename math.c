90f4cf5ec341742878c735962d39f8ca1c8f215d
MyraAshley <47799854+MyraAshley@users.noreply.github.com>
2019-05-01 16:01:31 -0400

    Update math.c

--- math.c
+++ math.c
@@ -1,26 +1,36 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 #include <math.h>
 
-int v; 
-int r; 
-int i; 
-int x; 
-int main(void){
-    printf ("what is the voltage?\n");
-    scanf ("%d",&v);
-    printf ("what is the resistance\n");
-    scanf ("%d",&r);
-    printf ("what is the current\n");
-     scanf ("%d",&i);
-    printf ("what is the unknown?\n");
-     scanf ("%d",&x);
-    if ( v ==  x){
-          v = i * r;}
-    if (r ==  x){
-            r = v/i;}
-    if ( i ==  x){
-            i = v/r;}
+int main(){
+    int option;
+    float V, I, R, result;
+    printf ("Press 1 to calculate the voltage; press 2 to calculate the resistance; press 3 to calculate the electric current\n");//user will press any of the numbers given to calculate the answer they need
+    scanf("%d", &option);
+    if ( option ==  1){
+        printf("What is the resistance in ohms?\n");//user inputs the resistance in ohms
+        scanf("%f", &R);
+        printf("What is the electric current in amps?\n");//user inputs the current in amps
+        scanf("%f", &I);
+        result = R * I;//the equation to calculate voltage 
+        printf("The voltage is %0.2f volts\n", result);//gives the answer in decimal form and in the unit of volts
+    }
+    else if (option ==  2){
+        printf("What is the voltage in volts?\n");//user inputs the voltage in volts
+        scanf("%f", &V);
+        printf("What is the electric current amps?\n");//user inputs the electric current in amps
+        scanf("%f", &I);
+        result = V / I;//equation to find the resistance
+        printf("The resistance is %0.2f omhs\n", result);//gives the answer in decimal form and in the unit of ohms 
+    }
+    else if ( option ==  3){
+        printf("What is the voltage in volts?\n");//user inputs the voltage in volts
+        scanf("%f", &V);
+        printf("What is the resistance in ohms?\n");//user inputs the resistance in ohms 
+        scanf("%f", &R);
+        result = V / R;//equation to calculate the electric current 
+        printf("The electric current is %0.2f amps\n", result);//gives the answer in decimal form and in the unit of amps
+    }
+    return 0;
 }
-
 
